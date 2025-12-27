5e46d069bf22e2ca7fb443fbef24fb7e0e69b1a3
Divyanshu2003D <divyanshu200372@gmail.com>
2025-12-27 15:50:34 +0530

    Clean up: Tracking only task folders using gitignore

--- DOM/script.js
+++
@@ -1,8 +0,0 @@
-var h1= document.querySelector('h1')
-console.log(h1);
-h1.innerHTML = 'Dom padhlo guys!'
-h1.style.color = 'red'
-h1.style.backgroundColor = 'white'
-h1.addEventListener('click', function(){
-    console.log("YO ham padh rahe hai")
-})
